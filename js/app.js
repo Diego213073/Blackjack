9335424941fdd4e017ea078a9f6f23c97bd65446
Diego Roble <diego.roble@zabud.com.co>
2021-12-02 23:46:25 -0500

    creación de una nueva carta en el html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 let cartas = [];
 const tiposCartas = ['C', 'D', 'H', 'S'];
 const tiposEspeciales = ['A', 'J', 'Q', 'K'];
+let puntosJugador = 0;
+let puntosPC = 0;
+const span = document.querySelectorAll('span');
+const jugadorCartas = document.querySelector('#jugadorCartas');
 
-
-
-document.querySelector('.nuevo-juego').addEventListener('click', () => crearCartas());
 
 
 const crearCartas = () => {
@@ -22,19 +23,17 @@ const crearCartas = () => {
         }
     }
 
-    console.log(valorCarta(tomarUnaCarta(cartas)));
 
 };
 
 
-const tomarUnaCarta = (arr) => {
-    const random = Math.round(Math.random() * arr.length);
+const tomarUnaCarta = () => {
+    const random = Math.round(Math.random() * cartas.length);
     let carta = 'No hay cartas disponibles';
-    arr.forEach((element, index) => {
+    cartas.forEach((element, index) => {
         if (index === random) {
             carta = element;
-            arr.splice(index, 1);
-            
+            cartas.splice(index, 1);
         }
     });
     return carta;
@@ -42,9 +41,29 @@ const tomarUnaCarta = (arr) => {
 
 
 const valorCarta = (carta) => {
-    console.log(`Carta entrante: ${carta}`);
     let valor = carta.substring(0, carta.length - 1);
     return (isNaN(valor) ? (valor === 'A' ? 11 : 10) : parseInt(valor));
 };
 
+crearCartas();
 
+
+const pedirCarta = document.querySelector('.pedir');
+
+
+pedirCarta.addEventListener('click', () => {
+
+    const carta = tomarUnaCarta();
+    puntosJugador += valorCarta(carta);
+    span[0].innerText = puntosJugador;
+    jugadorCartas.appendChild(nuevaCarta(carta));
+
+});
+
+const nuevaCarta = (carta) =>{
+    const nuevaCarta = document.createElement('img');
+    nuevaCarta.src = 'static/cartas/'+carta+'.png';
+    nuevaCarta.alt = "carta";
+    nuevaCarta.width = "90";
+    return nuevaCarta;
+};
